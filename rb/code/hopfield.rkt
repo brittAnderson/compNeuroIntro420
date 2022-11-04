f957e4e3cce4759c86b4c44e230344dda9b49918
Britt Anderson <britt@uwaterloo.ca>
2022-11-04 14:49:57 -0400

    made hopfield code with visualization

--- rb/code/hopfield.rkt
+++ rb/code/hopfield.rkt
@@ -3,6 +3,8 @@
 (require math/matrix
          math/array
          racket/random
+         racket/snip
+         racket/draw
          "outer.rkt")
 
 
@@ -23,101 +25,82 @@
                  (if (= i j) 0.0 (matrix-ref m i j)))))
                  
 
-(define (hop-mk-wts patt-list)
+(define (hf-mk-wts patt-list)
   (let*-values ([(r c) (matrix-shape (first patt-list))]
                 [(rc) (* r c)])
     (for/fold ([m (make-matrix (* r r) (* c c) 0)]
-               #:result (matrix-scale (zero-diag m) (/ 1 (length patt-list))))
+               #:result (array->mutable-array (matrix-scale (zero-diag m) (/ 1 (length patt-list)))))
               ([p patt-list])
       (let ([p-col (array-reshape p (vector (* r c) 1))])
         (matrix+ m (outer-prod p-col p-col))))))
     
-   IAMHERE 
+
+(define (hf-calc-new-node-value r c #:threshold [threshold 0.0])
+  (if (< threshold (matrix-ref (matrix* r c) 0 0)) 1.0 -1.0))
+
+(define (hf-async1 wts pattern)
+  (let-values ([(working-p) (mutable-array-copy pattern)]
+               [(test-p)    (mutable-array-copy pattern)]
+               [(r c) (matrix-shape wts)]
+               [(b?) #f])
+    (for ([i (in-naturals)])
+      #:break b?
+      (for ([rr (shuffle (range r))])
+        (array-set! working-p (vector rr 0) (hf-calc-new-node-value (matrix-row wts rr) working-p))
+        (set! b? (matrix= working-p test-p))
+        (set! test-p working-p)))
+    working-p))
+
+(define (hf-loop-patts wts patterns)
+  (for/fold ([trained-patterns (list )]
+             #:result (reverse trained-patterns))
+            ([p patterns])
+    (let-values ([(r c) (matrix-shape p)])
+      (cons
+       (array-reshape (hf-async1 wts (array->mutable-array (array-reshape p (vector (array-size p) 1)))) (vector r c)) trained-patterns))))
+
     
-; #+BEGIN_SRC python :session *hop* :tangle yes
-;   def hopMkWts(patterns):
-;       w = np.zeros(list(map((lambda x: x**2),patterns[0].shape)))
-;       for p in patterns:
-;           w = w + np.outer(p,p)
-;       w = 1.0/len(patterns) * w
-;       np.fill_diagonal(w,0)
-;       return(w)
-; #+END_SRC
-; 
-; #+RESULTS:
-; 
-; #+BEGIN_SRC python :session *hop* :tangle yes
-;   def hopLoop(patt,wts):
-;       workingpatt = deepcopy(patt)
-;       testpatt    = deepcopy(patt)
-;       while True:
-;           rws = list(range(patt.shape[0]))
-;           cls = list(range(patt.shape[1]))
-;           r.shuffle(rws)
-;           r.shuffle(cls)
-;           linpatt = np.reshape(workingpatt,(1,workingpatt.size))
-;           for rw in rws:
-;               for cl in cls:
-;                   workingpatt[rw][cl] = 1.0 if np.dot(linpatt,wts[rw*len(rws) + cl]) > 0 else -1.0
-;           break
-;                         #          if (np.all(testpatt == inpatt)): break
-;       return(workingpatt)
-; #+END_SRC
-; 
-; #+RESULTS:
-; 
-; ****** Function to Loop Asynchronously Through all the Elements of a
-; Pattern Until Output is Stable
-;        :PROPERTIES:
-;        :CUSTOM_ID: function-to-loop-asynchronously-through-all-the-elements-of-a-pattern-until-output-is-stable
-;        :END:
-; 
-; ****** A Function to Visualize the Output
-;        :PROPERTIES:
-;        :CUSTOM_ID: a-function-to-visualize-the-output
-;        :END:
-; 
-; #+BEGIN_SRC python :session *hop* :tangle yes
-;   def hopPlot(ins,outs): 
-;       r = len(ins)
-;       c = 3
-;       pltcntr = 1
-;       for i in range(r):
-;           p.subplot(r,3,pltcntr)
-;           p.imshow(ins[i])
-;           p.subplot(r,3,(pltcntr+1))
-;           p.imshow(outs[i])
-;           p.subplot(r,3,(pltcntr+2))
-;           p.imshow(ins[i]-outs[i])
-;           pltcntr = pltcntr+3
-;       return(p)
-; #+END_SRC
-; 
-; #+RESULTS:
-; 
-; ****** Example of How to Use
-;        :PROPERTIES:
-;        :CUSTOM_ID: example-of-how-to-use
-;        :END:
-; 
-; #+BEGIN_SRC python :session *hop* :tangle yes
-;   myn,mysz = hopGenSzN()
-;   myps = hopMkPatts(myn,mysz)
-;   w = hopMkWts(myps)
-;   outps = []
-;   for inp in myps:
-;       op = hopLoop(inp,w)
-;       outps.append(op)
-; #+END_SRC
-; 
-; #+RESULTS:
-; 
-; #+BEGIN_SRC python :session *hop* :tangle yes :results graphics file :file "hop.png"
-;   myp = hopPlot(myps,outps)
-; #+END_SRC
-; 
-; #+RESULTS:
-; 
-; file:8a1284b97d78ce1c4330d596fa0ccc6873678a1d.png]]
-; 
+(define (draw-one-pattern p dc scale shift-row shift-col)
+  (let* ([r (matrix-num-rows p)]
+         [c (matrix-num-cols p)])
+    (for ([ci (in-range c)]
+          [cc (in-range shift-col (+ shift-col (* c scale)) scale)])
+      (for ([rj (in-range r)]
+            [rr (in-range shift-row (+ shift-row (* r scale)) scale)])
+      (match (matrix-ref p ci rj)
+        [1.0 (send dc set-brush "red" 'solid)]
+        [-1.0 (send dc set-brush "green" 'solid)])
+      (send dc set-pen "white" 1 'transparent)
+      (send dc draw-rectangle cc rr
+            scale scale)))))
+
+(define (draw-multiple-pattern ps ts dc scale)
+  (for ([p ps]
+        [i (in-range (length ps))]
+        [t ts])
+    (let-values ([(r c) (matrix-shape p)])
+      (draw-one-pattern p dc scale (+ (* i scale) (* i (* scale r))) 0)
+      (draw-one-pattern t dc scale (+ (* i scale) (* i (* scale r))) (+ (* scale c) (* scale c))))))
+
+
+(define (hf-draw-wrapper ps ts #:scale [scale 10])
+  (let* ([rs (matrix-num-rows (first ps))]
+         [cs (matrix-num-cols (first ps))]
+         [pn (length ps)]
+         [img-x (* cs scale 3)]
+         [img-y (+ (* scale (- pn 1)) (* rs scale pn)) ]
+         [target (make-bitmap img-x img-y)]
+         [dc (new bitmap-dc% [bitmap target])])
+    (draw-multiple-pattern ps ts dc scale) 
+    (make-object image-snip% target)))
+
+; Example of how to use
+; Make your test patterns
+; (define test (hf-make-n-patts-size-m 4 16))
+; Then make the weights from the test patterns
+; (define wts (hf-mk-wts test))
+; Then train
+; (define outputs (hf-loop-patts wts test))
+; Then visualize the results
+; (hf-draw-wrapper test outputs #:scale 30)
 
--- rb/code/outer.rkt
+++ rb/code/outer.rkt
@@ -15,5 +15,5 @@
 
 (define (outer-prod v1 v2)
   (matrix* (if (col-matrix? v1) v1 (matrix-transpose v1))
-           (if (row-matrix? v2) v2 (matrix-transpose v2)))
+           (if (row-matrix? v2) v2 (matrix-transpose v2))))
 
