3aef71d4cbbaf8b3ef247ee5bca95fb6d6453821
Britt Anderson <britt@uwaterloo.ca>
2024-12-31 12:13:09 -0500

    add no parts book mode got off internet somewhere

---
+++ w2025/book-noparts.el
@@ -0,0 +1,9 @@
+(add-to-list 'org-latex-classes
+           '("book-noparts"
+              "\\documentclass{book}"
+              ("\\chapter{%s}" . "\\chapter*{%s}")
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+              ("\\paragraph{%s}" . "\\paragraph*{%s}")
+              ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
