e3eb26080fd3d2527ea533343117df4237e3bb4a
Britt Anderson <britt@uwaterloo.ca>
2026-01-21 06:15:34 -0500

    making module so I can import all for debugging testing to ghci

---
+++ w2026/src/Dev.hs
@@ -0,0 +1,19 @@
+module Dev
+  (module Automata
+  , module BusyBeaver
+  , module HandH
+  , module IandF
+  , module MyMats
+  , module Perceptron
+  , module Spring 
+  , module Sqrt
+  ) where
+
+import Automata
+import BusyBeaver
+import HandH
+import IandF
+import MyMats
+import Perceptron
+import Spring
+import Sqrt
